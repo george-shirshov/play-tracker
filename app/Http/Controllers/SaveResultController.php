2b6ac49dde908e3690bbec7bb4cfeff3d4e89615
Georgy <shirshov.gera@bk.ru>
2024-01-14 02:09:55 +0300

    Добавлен сервис для сохранения результатов прохождения игры

--- app/Http/Controllers/SaveResultController.php
+++ app/Http/Controllers/SaveResultController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Builder\Builder;
+use App\Data\Result\ResultRequest;
+use App\Services\ResultService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
 class SaveResultController extends Controller
 {
-    public function __invoke(): string
+    public function __construct(
+        private readonly ResultService $resultService,
+        private readonly Builder $builder,
+    ) {
+    }
+
+    public function __invoke(Request $request): JsonResponse
     {
-        return 'результаты сохранены';
+        $body = (object)$request->json()->all();
+
+        /** @var ResultRequest $resultRequest */
+        $resultRequest = $this->builder->build($body, ResultRequest::class);
+
+        $this->resultService->saveResultFromRequest($resultRequest);
+
+        return response()->json();
     }
 }
---
+++ app/Services/ResultService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Data\Result\ResultRequest;
+use App\Data\Result\ResultViewModel;
+use App\Repositories\MemberRepository;
+use App\Repositories\ResultRepository;
+
+readonly class ResultService
+{
+    public function __construct(
+        private ResultRepository $resultRepository,
+        private MemberRepository $memberRepository,
+    ) {
+    }
+
+    public function saveResultFromRequest(ResultRequest $resultRequest): void
+    {
+        $resultViewModel = new ResultViewModel();
+
+        if (!empty($resultRequest->email)) {
+            $memberModel = $this->memberRepository->getByEmail($resultRequest->email);
+            $resultViewModel->memberId = $memberModel->getId();
+        }
+
+        $resultViewModel->milliseconds = $resultRequest->milliseconds;
+
+        $this->resultRepository->save($resultViewModel);
+    }
+}
