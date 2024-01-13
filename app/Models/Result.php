de08d34c45852fc81a0fb7a82fc252c8ff48be3d
Georgy <shirshov.gera@bk.ru>
2024-01-14 01:38:15 +0300

    Добавлены сеттеры модели Result

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Result extends Model
 {
     use HasFactory;
+
+    public function setMemberId(?int $value): void
+    {
+        $this->member_id = $value;
+    }
+
+    public function setMilliseconds(int $value): void
+    {
+        $this->milliseconds = $value;
+    }
 }
