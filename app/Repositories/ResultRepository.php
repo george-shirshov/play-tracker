1ce683ebfb272c4d476c5a5de41da7158219790b
Georgy <shirshov.gera@bk.ru>
2024-01-14 01:51:52 +0300

    Добавлен класс репозиторий ResultRepository с методом save

--- app/Repositories/ResultRepository.php
+++ app/Repositories/ResultRepository.php
@@ -2,15 +2,16 @@
 
 namespace App\Repositories;
 
+use App\Data\Result\ResultViewModel;
 use App\Models\Result;
 
 class ResultRepository
 {
-    public function save(?int $memberId, int $milliseconds): void
+    public function save(ResultViewModel $viewModel): void
     {
         $result = new Result();
-        $result->setMemberId($memberId);
-        $result->setMilliseconds($milliseconds);
+        $result->setMemberId($viewModel->memberId);
+        $result->setMilliseconds($viewModel->milliseconds);
 
         $result->save();
     }
