5dc3f7c69ddcbc17e85df54032aa38608fe58459
Georgy <shirshov.gera@bk.ru>
2024-01-14 18:50:10 +0300

    Добавлены DTO классы ответа

---
+++ app/Data/Top/ResponseData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Data\Top;
+
+class ResponseData
+{
+    /** @var TopData[] */
+    public array $top;
+
+    public ?TopData $self = null;
+}
---
+++ app/Data/Top/TopData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Data\Top;
+
+class TopData
+{
+    public string $email;
+    public int $place;
+    public int $milliseconds;
+}
